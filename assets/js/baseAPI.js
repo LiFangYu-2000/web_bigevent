0a6d8bcfb25c512ac3f66918c256212677f23f19
Tom <947435372@qq.com>
2021-08-14 16:15:19 +0800

    index 已完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,15 @@
 $.ajaxPrefilter(function (options) {
   options.url = "http://www.liulongbin.top:3008" + options.url;
-  console.log(options.url);
+  //统一为有权限的接口 设置headers 请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+  options.complete = function (response) {
+    if (response.responseJSON.code == 1) {
+      localStorage.removeItem("token");
+      location.href = "/login.html";
+    }
+  };
 });
